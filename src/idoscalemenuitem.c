2cffa8580d3f658adce63aa5ff7ced8a14d9c509
Cody Russell <crussell@canonical.com>
2010-03-04 16:46:32 +0000

    Make 'ido-offscreen-scale' the name of the GtkOffscreenWindow, not the GtkScale.

--- src/idoscalemenuitem.c
+++ src/idoscalemenuitem.c
@@ -306,9 +306,9 @@ ido_scale_menu_item_constructor (GType                  type,
   priv = GET_PRIVATE (object);
 
   priv->offscreen = gtk_offscreen_window_new ();
+  gtk_widget_set_name (priv->offscreen, "ido-offscreen-scale");
 
   priv->scale = gtk_hscale_new_with_range (0.0, 100.0, 1.0);
-  gtk_widget_set_name (priv->scale, "ido-offscreen-scale");
   gtk_scale_set_draw_value (GTK_SCALE (priv->scale), FALSE);
   gtk_widget_show (priv->scale);
   gtk_widget_show (priv->offscreen);
