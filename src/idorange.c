f3893ee568a61cb9f893502113826a29e601e807
Cody Russell <crussell@canonical.com>
2010-08-03 14:47:05 -0500

    Fix for armel build.  Patch by David Sugar.

--- src/idorange.c
+++ src/idorange.c
@@ -179,7 +179,7 @@ ido_range_init (IdoRange *range)
  * Creates a new #IdoRange widget.
  **/
 GtkWidget *
-ido_range_new (GtkAdjustment *adj,
+ido_range_new (GtkObject     *adj,
                IdoRangeStyle  style)
 {
   g_return_val_if_fail (GTK_IS_ADJUSTMENT (adj), NULL);
