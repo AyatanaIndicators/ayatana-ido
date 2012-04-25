7db7f0e734e5c4d7dbc46fb415b8c60c3c570f8e
Ted Gould <ted@gould.cx>
2012-04-25 14:12:59 -0500

    Now we're allocating a calendar

--- tests/gtest-menuitems.cpp
+++ tests/gtest-menuitems.cpp
@@ -1,8 +1,26 @@
 
-#include <glib-object.h>
+#include <gtk/gtk.h>
 #include <gtest/gtest.h>
+#include "idocalendarmenuitem.h"
 
-TEST(StartTesting, DummyTest) {
-	g_type_init();
-	EXPECT_TRUE(true);
+class TestMenuitems : public ::testing::Test
+{
+public:
+	TestMenuitems()
+	{
+		g_type_init();
+		return;
+	}
+};
+
+TEST_F(TestMenuitems, BuildCalendar) {
+	GtkWidget * cal = ido_calendar_menu_item_new();
+
+	EXPECT_TRUE(cal != NULL);
+	EXPECT_TRUE(IDO_IS_CALENDAR_MENU_ITEM(cal));
+	EXPECT_TRUE(GTK_IS_MENU_ITEM(cal));
+
+	g_object_ref_sink(cal);
+	g_object_unref(cal);
+	return;
 }
