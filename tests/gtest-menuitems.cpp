072a5ab6d97eda7911de3c8ec843f9368af44c61
Ted Gould <ted@gould.cx>
2012-04-25 14:22:27 -0500

    Now we can realize the menu item to make sure it doesn't mess that up

--- tests/gtest-menuitems.cpp
+++ tests/gtest-menuitems.cpp
@@ -8,7 +8,9 @@ class TestMenuitems : public ::testing::Test
 public:
 	TestMenuitems()
 	{
-		g_type_init();
+		gint argc = 0;
+		gchar * argv[] = {NULL};
+		gtk_init(&argc, (gchar ***)&argv);
 		return;
 	}
 };
@@ -19,6 +21,15 @@ TEST_F(TestMenuitems, BuildCalendar) {
 	EXPECT_TRUE(cal != NULL);
 	EXPECT_TRUE(IDO_IS_CALENDAR_MENU_ITEM(cal));
 	EXPECT_TRUE(GTK_IS_MENU_ITEM(cal));
+
+	GtkWidget * menu = gtk_menu_new();
+	gtk_widget_show(menu);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menu), cal);
+
+	gtk_widget_show(cal);
+	gtk_widget_realize(cal);
+
+	EXPECT_TRUE(gtk_widget_get_realized(cal));
 
 	g_object_ref_sink(cal);
 	g_object_unref(cal);
