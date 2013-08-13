bb86413dd05a38b4879a109fbb52563a56e6ae5f
Ted Gould <ted@gould.cx>
2013-08-13 15:28:16 -0500

    Library functions taking GVariant params need to ref_sink() and unref() always. (reason #24 to not like GVariant)

--- src/idoactionhelper.c
+++ src/idoactionhelper.c
@@ -418,8 +418,12 @@ ido_action_helper_activate_with_parameter (IdoActionHelper *helper,
   g_return_if_fail (IDO_IS_ACTION_HELPER (helper));
   g_return_if_fail (parameter != NULL);
 
+  g_variant_ref_sink (parameter);
+
   if (helper->actions && helper->action_name)
     g_action_group_activate_action (helper->actions, helper->action_name, parameter);
+
+  g_variant_unref (parameter);
 }
 
 /**
