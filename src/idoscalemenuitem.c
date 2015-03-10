3bef6a1972f8c09b954b9733d79dde42dd86650b
Lars Uebernickel <lars@uebernic.de>
2015-03-10 12:38:25 +0100

    idoscalemenuitem: set focus flag on scale
    So that it gets themed correctly when the menu item is selected.

--- src/idoscalemenuitem.c
+++ src/idoscalemenuitem.c
@@ -472,6 +472,7 @@ ido_scale_menu_item_select (GtkMenuItem *item)
   IdoScaleMenuItemPrivate *priv = GET_PRIVATE (item);
 
   priv->has_focus = TRUE;
+  gtk_widget_set_state_flags (priv->scale, GTK_STATE_FLAG_FOCUSED, FALSE);
 
   GTK_MENU_ITEM_CLASS (ido_scale_menu_item_parent_class)->select (item);
 }
@@ -482,6 +483,7 @@ ido_scale_menu_item_deselect (GtkMenuItem *item)
   IdoScaleMenuItemPrivate *priv = GET_PRIVATE (item);
 
   priv->has_focus = FALSE;
+  gtk_widget_unset_state_flags (priv->scale, GTK_STATE_FLAG_FOCUSED);
 
   GTK_MENU_ITEM_CLASS (ido_scale_menu_item_parent_class)->deselect (item);
 }
